3b699d3395c759b154b7b5feef21bba942578360
susannaaz <susanna.azzoni@gmail.com>
2020-06-23 17:10:31 +0100

    plotting scripts

---
+++ plotting_scripts/compare_cls_Pysm_vs_Gaus.py
@@ -0,0 +1,177 @@
+import pymaster as nmt
+import healpy as hp
+import numpy as np
+import os
+import scipy.constants as constants
+from scipy import optimize
+import matplotlib.pyplot as plt
+from optparse import OptionParser
+from optparse import OptionParser
+
+# Masked amplitude maps to power spectra
+def map2dl(mask, maps, i, j, k, l):
+    # Read amplitude maps
+    q_nu1, u_nu1 = hp.ud_grade(hp.read_map(maps, verbose=False, field=[i,j]), nside_out=512) 
+    q_nu2, u_nu2 = hp.ud_grade(hp.read_map(maps, verbose=False, field=[k,l]), nside_out=512) 
+    nside = hp.npix2nside(len(q_nu1))
+    # Binary mask
+    mask_binary = np.ones_like(mask)
+    mask_binary[mask<=0] = 0
+    # Mask fields
+    q_nu1 *= mask_binary
+    u_nu1 *= mask_binary
+    q_nu2 *= mask_binary
+    u_nu2 *= mask_binary
+    # Binning
+    # nlb = beam_width = 10 ells
+    bn = nmt.NmtBin(nside, nlb=10, is_Dell=True)
+    # Field
+    field1 = nmt.NmtField(mask, [q_nu1, u_nu1])
+    field2 = nmt.NmtField(mask, [q_nu2, u_nu2])
+    # M-C-M (Mode Coupling Matrix)
+    wsp = nmt.NmtWorkspace()
+    if os.path.isfile("mcm.fits"):
+        wsp.read_from("mcm.fits")
+    else:
+        wsp.compute_coupling_matrix(field1, field2, bn)
+        wsp.write_to("mcm.fits")
+    # C_ell
+    dl_ee, dl_eb, dl_be, dl_bb = wsp.decouple_cell(nmt.compute_coupled_cell(field1, field2))
+    ell_eff = bn.get_effective_ells()
+    return ell_eff, dl_ee, dl_bb
+
+# Read mask
+mask = hp.read_map("./data/masks_SAT.fits", verbose=False)
+
+# Maps of synchrotron, dust and sync+dust
+maps_gaus_s='./FITS_sim_ns256_seed1000_stdd0_stds0_gdm3.0_gsm3.0_msk_E_B_nu0d353_nu0s23_sync_Ad28/maps_sky_signal.fits'
+maps_pysm_s='./FITS_sim_ns256_seed1000_stdd0_stds0_gdm3.0_gsm3.0_msk_E_B_pysmBetas_realAmps_nu0d353_nu0s23_sync_Ad5/maps_sky_signal.fits'
+maps_gaus_d='./FITS_sim_ns256_seed1000_stdd0_stds0_gdm3.0_gsm3.0_msk_E_B_nu0d353_nu0s23_dust_Ad28/maps_sky_signal.fits'
+maps_pysm_d='./FITS_sim_ns256_seed1000_stdd0_stds0_gdm3.0_gsm3.0_msk_E_B_pysmBetas_realAmps_nu0d353_nu0s23_dust_Ad5/maps_sky_signal.fits'
+maps_gaus_ds='./FITS_sim_ns256_seed1000_stdd0_stds0_gdm3.0_gsm3.0_msk_E_B_nu0d353_nu0s23_dust_sync_Ad28/maps_sky_signal.fits'
+maps_pysm_ds='./FITS_sim_ns256_seed1000_stdd0_stds0_gdm3.0_gsm3.0_msk_E_B_pysmBetas_realAmps_nu0d353_nu0s23_dust_sync_Ad5/maps_sky_signal.fits'
+
+if not os.path.isfile("Outputs_compare_cls_Pysm_vs_Gaus/dls_gaus_s.txt") and os.path.isfile("Outputs_compare_cls_Pysm_vs_Gaus/dls_gaus_d.txt") and os.path.isfile("Outputs_compare_cls_Pysm_vs_Gaus/dls_gaus_ds.txt") and os.path.isfile("Outputs_compare_cls_Pysm_vs_Gaus/ls.txt"):
+    print('oops!')
+    ##open("Outputs_compare_cls_Pysm_vs_Gaus/ls.txt", 'w')
+    ##open('Outputs_compare_cls_Pysm_vs_Gaus/dls_gaus_s.txt', 'w')                                                                                             
+    ##open('Outputs_compare_cls_Pysm_vs_Gaus/dls_gaus_d.txt', 'w')                                                                                             
+    ##open('Outputs_compare_cls_Pysm_vs_Gaus/dls_gaus_ds.txt', 'w')                                                                                       
+    ##open('Outputs_compare_cls_Pysm_vs_Gaus/dls_pysm_s.txt', 'w')                                                                                             
+    ##open('Outputs_compare_cls_Pysm_vs_Gaus/dls_pysm_d.txt', 'w')                                                                                             
+    ##open('Outputs_compare_cls_Pysm_vs_Gaus/dls_pysm_ds.txt', 'w')
+    ##dls_gaus_s = []
+    ##dls_pysm_s = []
+    ##dls_gaus_d = []
+    ##dls_pysm_d = []
+    ##dls_gaus_ds = []
+    ##dls_pysm_ds = []
+    ##ls = []
+    ##for i in np.arange(0, 12, 2):
+    ##    j=i+1
+    ##    for k in np.arange(i, 12, 2):
+    ##        l=k+1
+    ##        ''' Write the dls arrrays to disk
+    ##        order: 1x1, 1x2, 1x3, 1x4, 1x5, 1x6,
+    ##                    2x2, 2x3, 2x4, 2x5, 2x6,
+    ##                         3x3, 3x4, 3x5, 3x6,
+    ##                              4x4, 4x5, 4x6,
+    ##                                   5x5, 5x6,
+    ##                                        6x6.'''        
+    ##        print('Selecting map fields')
+    ##        print('i j k l')
+    ##        print(i, j, k, l)
+    ##        print('Computing Dl at cross frequencies')        
+    ##        print('nu1 nu2')
+    ##        print((i+2)//2, (k+2)//2)
+    ##        print('-------------------')
+    ##    
+    ##        ell_eff, dl_ee_pysm_s, dl_bb_pysm_s = map2dl(mask, maps_pysm_s, i,j,k,l)
+    ##        _, dl_ee_gaus_s, dl_bb_gaus_s = map2dl(mask, maps_gaus_s, i,j,k,l)
+    ##    
+    ##        _, dl_ee_pysm_d, dl_bb_pysm_d = map2dl(mask, maps_pysm_d, i,j,k,l)
+    ##        _, dl_ee_gaus_d, dl_bb_gaus_d = map2dl(mask, maps_gaus_d, i,j,k,l)
+    ##    
+    ##        _, dl_ee_pysm_ds, dl_bb_pysm_ds = map2dl(mask, maps_pysm_ds,i,j,k,l)
+    ##        _, dl_ee_gaus_ds, dl_bb_gaus_ds = map2dl(mask, maps_gaus_ds, i,j,k,l)
+    ##
+    ##        ells=ell_eff
+    ##        dl2cl = np.ones(len(ells))
+    ##        dl2cl[1:] = 2*np.pi/(ells[1:]*(ells[1:]+1.))
+    ##        cl2dl = (ells*(ells+1.))/(2*np.pi)
+    ##        #cl_bb_gaus *= dl2cl
+    ##        #cl_bb_pysm *= dl2cl
+    ##    
+    ##        dls_gaus_s.append(dl_bb_gaus_s)
+    ##        dls_pysm_s.append(dl_bb_pysm_s)
+    ##        dls_gaus_d.append(dl_bb_gaus_d)
+    ##        dls_pysm_d.append(dl_bb_pysm_d)
+    ##        dls_gaus_ds.append(dl_bb_gaus_ds)
+    ##        dls_pysm_ds.append(dl_bb_pysm_ds)
+    ##        ls.append(ell_eff)
+    ##
+    ##ls = np.asarray(ls)
+    ##dls_gaus_s = np.asarray(dls_gaus_s)
+    ##dls_pysm_s = np.asarray(dls_pysm_s)
+    ##dls_gaus_d = np.asarray(dls_gaus_d)
+    ##dls_pysm_d = np.asarray(dls_pysm_d)
+    ##dls_gaus_ds = np.asarray(dls_gaus_ds)
+    ##dls_pysm_ds = np.asarray(dls_pysm_ds)
+    ##
+    ##np.savetxt("Outputs_compare_cls_Pysm_vs_Gaus/ls.txt", ls)
+    ##np.savetxt("Outputs_compare_cls_Pysm_vs_Gaus/dls_gaus_s.txt", dls_gaus_s)
+    ##np.savetxt("Outputs_compare_cls_Pysm_vs_Gaus/dls_pysm_s.txt", dls_pysm_s)
+    ##np.savetxt("Outputs_compare_cls_Pysm_vs_Gaus/dls_gaus_d.txt", dls_gaus_d)
+    ##np.savetxt("Outputs_compare_cls_Pysm_vs_Gaus/dls_pysm_d.txt", dls_pysm_d)
+    ##np.savetxt("Outputs_compare_cls_Pysm_vs_Gaus/dls_gaus_ds.txt", dls_gaus_ds)
+    ##np.savetxt("Outputs_compare_cls_Pysm_vs_Gaus/dls_pysm_ds.txt", dls_pysm_ds)
+else:
+    ls = np.loadtxt("Outputs_compare_cls_Pysm_vs_Gaus/ls.txt")
+    dls_gaus_s = np.loadtxt("Outputs_compare_cls_Pysm_vs_Gaus/dls_gaus_s.txt")
+    dls_pysm_s = np.loadtxt("Outputs_compare_cls_Pysm_vs_Gaus/dls_pysm_s.txt")
+    dls_gaus_d = np.loadtxt("Outputs_compare_cls_Pysm_vs_Gaus/dls_gaus_d.txt")
+    dls_pysm_d = np.loadtxt("Outputs_compare_cls_Pysm_vs_Gaus/dls_pysm_d.txt")
+    dls_gaus_ds = np.loadtxt("Outputs_compare_cls_Pysm_vs_Gaus/dls_gaus_ds.txt")
+    dls_pysm_ds = np.loadtxt("Outputs_compare_cls_Pysm_vs_Gaus/dls_pysm_ds.txt")
+
+# Plot results
+label_list = []
+for i in np.arange(0, 12, 2):
+    for k in np.arange(i, 12, 2):
+        label_list.append('%s_x_%s'%((i+2)//2,(k+2)//2))
+
+for i in np.arange(21):
+    labels = label_list[i]    
+    plt.figure()
+    plt.plot(ls[i][3:30], dls_gaus_s[i][3:30], 'r-', label='gaussian sim')
+    plt.plot(ls[i][3:30], dls_pysm_s[i][3:30], 'b-', label='pysm sim')
+    plt.title('Synchrotron'+label_list[i]) 
+    plt.loglog()
+    plt.xlabel('$\\ell$', fontsize=16)
+    plt.ylabel('$D_\\ell$', fontsize=16)
+    plt.legend(loc='upper right', ncol=2, labelspacing=0.1)
+    plt.savefig('Outputs_compare_cls_Pysm_vs_Gaus/dlbb_SIMsync_COMPARA_'+label_list[i]) 
+
+    plt.figure()
+    plt.plot(ls[i][3:30], dls_gaus_d[i][3:30], 'r-', label='gaussian sim')
+    plt.plot(ls[i][3:30], dls_pysm_d[i][3:30], 'b-', label='pysm sim')
+    plt.title('Dust'+label_list[i]) 
+    plt.loglog()
+    plt.xlabel('$\\ell$', fontsize=16)
+    plt.ylabel('$D_\\ell$', fontsize=16)
+    plt.legend(loc='upper right', ncol=2, labelspacing=0.1)
+    plt.savefig('Outputs_compare_cls_Pysm_vs_Gaus/dlbb_SIMdust_COMPARA_'+label_list[i]) 
+
+    plt.figure()
+    plt.plot(ls[i][3:30], dls_gaus_ds[i][3:30], 'r-', label='gaussian sim')
+    plt.plot(ls[i][3:30], dls_pysm_ds[i][3:30], 'b-', label='pysm sim')
+    plt.title('Dust + Synchrotron'+label_list[i]) 
+    plt.loglog()
+    plt.xlabel('$\\ell$', fontsize=16)
+    plt.ylabel('$D_\\ell$', fontsize=16)
+    plt.legend(loc='upper right', ncol=2, labelspacing=0.1)
+    plt.savefig('Outputs_compare_cls_Pysm_vs_Gaus/dlbb_SIMdustsync_COMPARA_'+label_list[i]) 
+
+
+
+
---
+++ plotting_scripts/plotting.py
@@ -0,0 +1,101 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+dir0 = 'simulations_only_sync/sims_ns256_seed1000_lm383'
+dir1 = 'simulations_only_sync/sims_ns256_seed1001_lm383'
+dir2 = 'simulations_only_sync/sims_ns256_seed1002_lm383'
+dir3 = 'simulations_only_sync/sims_ns256_seed1003_lm383'
+dir4 = 'simulations_only_sync/sims_ns256_seed1004_lm383'
+dir5 = 'simulations_only_sync/sims_ns256_seed1005_lm383'
+dir6 = 'simulations_only_sync/sims_ns256_seed1006_lm383'
+dir7 = 'simulations_only_sync/sims_ns256_seed1007_lm383'
+dir8 = 'simulations_only_sync/sims_ns256_seed1008_lm383'
+dir9 = 'simulations_only_sync/sims_ns256_seed1009_lm383'
+
+ells = np.loadtxt(dir0+'/ells.txt')
+
+def cl(dirname, typ):
+    """ Import cl data from txt file """
+    cl = np.zeros([51, 6, 6])
+    for i_nu1 in range(6):  
+        for i_nu2 in range(i_nu1, 6):
+            if typ=='data_p0':
+                cl[:,i_nu1,i_nu2] = np.loadtxt(dirname+'/cl_d_b0p0_%d_%d.txt'% (i_nu1, i_nu2))
+            elif typ=='data_p2':
+                cl[:,i_nu1,i_nu2] = np.loadtxt(dirname+'/cl_d_b0p2_%d_%d.txt'% (i_nu1, i_nu2))
+            elif typ=='err_p0':
+                cl[:,i_nu1,i_nu2] = np.loadtxt(dirname+'/err_b0p0_%d_%d.txt'% (i_nu1, i_nu2))
+            elif typ=='err_p2':
+                cl[:,i_nu1,i_nu2] = np.loadtxt(dirname+'/err_b0p2_%d_%d.txt'% (i_nu1, i_nu2))
+            elif typ=='theory_p0':
+                cl[:,i_nu1,i_nu2] = np.loadtxt(dirname+'/cl_t_b0p0_%d_%d.txt'% (i_nu1, i_nu2))
+            elif typ=='theory_p2':
+                cl[:,i_nu1,i_nu2] = np.loadtxt(dirname+'/cl_t_b0p2_%d_%d.txt'% (i_nu1, i_nu2))
+    return cl
+
+def cl_mean(cl0, cl1, cl2, cl3, cl4, cl5, cl6, cl7, cl8, cl9):
+    """ Function to calculate mean of the 10 realizations """
+    cl_mean_01 = np.add(cl0, cl1)
+    cl_mean_23 = np.add(cl2, cl3)
+    cl_mean_45 = np.add(cl4, cl5)
+    cl_mean_67 = np.add(cl6, cl7)
+    cl_mean_89 = np.add(cl8, cl9)
+    cl_mean_0123 = np.add(cl_mean_01, cl_mean_23)
+    cl_mean_4567 = np.add(cl_mean_45, cl_mean_67)
+    cl_mean_01234567 = np.add(cl_mean_0123, cl_mean_4567)
+    cl_mean_all = np.add(cl_mean_89, cl_mean_01234567)
+    return cl_mean_all / 10
+ 
+# Calculate the mean cls for
+#
+# Data
+cl_d_b0p0_mean = cl_mean(cl(dir0, 'data_p0'), cl(dir1, 'data_p0'), cl(dir2, 'data_p0'), cl(dir3, 'data_p0'), cl(dir4, 'data_p0'), cl(dir5, 'data_p0'), cl(dir6, 'data_p0'), cl(dir7, 'data_p0'), cl(dir8, 'data_p0'), cl(dir9, 'data_p0')) 
+cl_d_b0p2_mean = cl_mean(cl(dir0, 'data_p2'), cl(dir1, 'data_p2'), cl(dir2, 'data_p2'), cl(dir3, 'data_p2'), cl(dir4, 'data_p2'), cl(dir5, 'data_p2'), cl(dir6, 'data_p2'), cl(dir7, 'data_p2'), cl(dir8, 'data_p2'), cl(dir9, 'data_p2'))
+#
+# Theory
+cl_t_b0p0_mean = cl_mean(cl(dir0, 'theory_p0'), cl(dir1, 'theory_p0'), cl(dir2, 'theory_p0'), cl(dir3, 'theory_p0'), cl(dir4, 'theory_p0'), cl(dir5, 'theory_p0'), cl(dir6, 'theory_p0'), cl(dir7, 'theory_p0'), cl(dir8, 'theory_p0'), cl(dir9, 'theory_p0'))
+cl_t_b0p2_mean = cl_mean(cl(dir0, 'theory_p2'), cl(dir1, 'theory_p2'), cl(dir2, 'theory_p2'), cl(dir3, 'theory_p2'), cl(dir4, 'theory_p2'), cl(dir5, 'theory_p2'), cl(dir6, 'theory_p2'), cl(dir7, 'theory_p2'), cl(dir8, 'theory_p2'), cl(dir9, 'theory_p2'))
+#
+# Errors
+err_b0p0_mean = cl_mean(cl(dir0, 'err_p0'), cl(dir1, 'err_p0'), cl(dir2, 'err_p0'), cl(dir3, 'err_p0'), cl(dir4, 'err_p0'), cl(dir5, 'err_p0'), cl(dir6, 'err_p0'), cl(dir7, 'err_p0'), cl(dir8, 'err_p0'), cl(dir9, 'err_p0'))
+err_b0p2_mean = cl_mean(cl(dir0, 'err_p2'), cl(dir1, 'err_p2'), cl(dir2, 'err_p2'), cl(dir3, 'err_p2'), cl(dir4, 'err_p2'), cl(dir5, 'err_p2'), cl(dir6, 'err_p2'), cl(dir7, 'err_p2'), cl(dir8, 'err_p2'), cl(dir9, 'err_p2'))
+
+# Plot stuff
+#
+# All plots together
+fig, ax = plt.subplots(6, 6, figsize=(38,40), sharex='col', sharey='row')
+#fig, ax = plt.subplots(6, 6, sharex='col', sharey='row')
+fig.subplots_adjust(hspace=0.9, wspace=0.7)
+for i_1 in range(6):  
+    for i_2 in range(i_1,6):
+        ax[i_1,i_2].set_title(str('%d x %d' % (i_1, i_2)), fontsize=9)
+        ax[i_1,i_2].errorbar(ells, cl_d_b0p0_mean[:,i_1,i_2], yerr=err_b0p0_mean[:,i_1,i_2],
+                     fmt='k.', label='No beta variations')
+        ax[i_1,i_2].plot(ells, cl_t_b0p0_mean[:,i_1,i_2], 'k--')
+        ax[i_1,i_2].errorbar(ells, cl_d_b0p2_mean[:,i_1,i_2], yerr=err_b0p2_mean[:,i_1,i_2],
+                     fmt='r.', label='0.2 beta variations')
+        ax[i_1,i_2].plot(ells, cl_t_b0p2_mean[:,i_1,i_2], 'r--')
+        ax[i_1,i_2].set_yscale('log')
+        #ax[i_1,i_2].set_xscale('log') 
+        ax[i_1,i_2].set_xlabel(str(r'$\ell$'), fontsize=8)
+        ax[i_1,i_2].set_ylabel(str(r'$D_\ell$'), fontsize=8)
+plt.savefig(f'plots/cls_only_sync_mean10sims/mean_all_logy.pdf')
+#
+# Single plots
+for i_1 in range(6):  
+    for i_2 in range(i_1,6):
+        plt.figure()
+        plt.title('%d x %d' % (i_1, i_2), fontsize=14)
+        plt.errorbar(ells, cl_d_b0p0_mean[:,i_1,i_2], yerr=err_b0p0_mean[:,i_1,i_2],
+                     fmt='k.', label='No beta variations')
+        plt.plot(ells, cl_t_b0p0_mean[:,i_1,i_2], 'k--')
+        plt.errorbar(ells, cl_d_b0p2_mean[:,i_1,i_2], yerr=err_b0p2_mean[:,i_1,i_2],
+                     fmt='r.', label='0.2 beta variations')
+        plt.plot(ells, cl_t_b0p2_mean[:,i_1,i_2], 'r--')
+        plt.xlabel(r'$\ell$', fontsize=16)
+        plt.ylabel(r'$D_\ell$', fontsize=16)
+        #plt.yscale('log')
+        #plt.xscale('log')
+        plt.legend()
+        plt.savefig(f'plots/cls_only_sync_mean10sims/mean_only_sync_{i_1}_{i_2}_logy.pdf')
+
